d3d176d9a5a09d24e0e2ac6bb4ee9627ddca5e06
Hamza Eraoui <hamzaprofessionnel1234@gmail.com>
2022-04-05 21:11:36 +0000

    improve style quality :)

--- src/components/AppSidebar.js
+++ src/components/AppSidebar.js
@@ -30,8 +30,7 @@ const AppSidebar = () => {
       }}
     >
       <CSidebarBrand className="d-none d-md-flex" to="/">
-        <CIcon className="sidebar-brand-full" icon={logoNegative} height={35} />
-        <CIcon className="sidebar-brand-narrow" icon={sygnet} height={35} />
+          <strong style={{fontSize:"20px"}}>Resources Management</strong>
       </CSidebarBrand>
       <CSidebarNav>
         <SimpleBar>
--- src/services/config.js
+++ src/services/config.js
@@ -2,3 +2,6 @@
 export const API_URL = "http://localhost:8081";
 
 export const TEACHER_API_URL = "http://localhost:8084";
+
+
+
