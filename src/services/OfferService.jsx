236b8eec10f1064917dd6edde1ee41ef85c646b1
Admin <Admin@PC-ADMIN>
2022-05-22 16:13:49 +0100

    offer

--- src/services/OfferService.jsx
+++ src/services/OfferService.jsx
@@ -13,8 +13,24 @@ export const getDemands = async () => {
   return demands
 }
 
+export const getDemand = async (id) => {
+  let { data: demand } = await axios.get(RESOURCES_API_URL+'/offer/demand/' + id)
+
+  return demand
+}
+
 export const AddOffer = async (offer) => {
   await axios.post(RESOURCES_API_URL+'/offer/addtolist',offer)
+  .then((response) => {
+    return response
+  })
+  .catch((error) => {
+    return error
+  })
+}
+
+export const UpdateStatus = async (demand) => {
+  await axios.post(RESOURCES_API_URL+'/offer/updatestatus',demand)
   .then((response) => {
     return response
   })
